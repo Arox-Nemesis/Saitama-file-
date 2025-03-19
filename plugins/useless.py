a95505329eacb50e27181108d9c1f0db7b4f1ff3
Arox-Nemesis <135242263+Arox-Nemesis@users.noreply.github.com>
2025-03-19 11:52:20 +0530

    Update useless.py

--- plugins/useless.py
+++ plugins/useless.py
@@ -3,13 +3,12 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram import filters
 from config import ADMINS, BOT_STATS_TEXT, USER_REPLY_TEXT
 from datetime import datetime
-from helper_func import get_readable_time
 
 @Bot.on_message(filters.command('stats') & filters.user(ADMINS))
 async def stats(bot: Bot, message: Message):
     now = datetime.now()
     delta = now - bot.uptime
-    time = get_readable_time(delta.seconds)
+    time = str(delta).split('.')[0]  # Formats time as HH:MM:SS
     await message.reply(BOT_STATS_TEXT.format(uptime=time))
 
 @Bot.on_message(filters.private & filters.incoming)
@@ -21,4 +20,3 @@ async def useless(_, message: Message):
             ]
         )
         await message.reply_text(USER_REPLY_TEXT, reply_markup=keyboard)
-
