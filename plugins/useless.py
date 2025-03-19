00ce2c73df0df199629ed43c139395933a54aca1
Arox-Nemesis <135242263+Arox-Nemesis@users.noreply.github.com>
2025-03-19 17:51:47 +0530

    Update useless.py

--- plugins/useless.py
+++ plugins/useless.py
@@ -3,7 +3,6 @@ from pyrogram.types import Message
 from pyrogram import filters
 from config import ADMINS, BOT_STATS_TEXT, USER_REPLY_TEXT
 from datetime import datetime
-from helper_func import get_readable_time
 
 @Bot.on_message(filters.command('stats') & filters.user(ADMINS))
 async def stats(bot: Bot, message: Message):
