0241ff9b71320c225272babfa764c7468feeda1b
Arox-Nemesis <135242263+Arox-Nemesis@users.noreply.github.com>
2025-03-20 17:37:34 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -84,7 +84,7 @@ async def start_command(client: Client, message: Message):
             for msg in messages:
                 if bool(CUSTOM_CAPTION) & bool(msg.document):
                     CUSTOM_CAPTION.format(previouscaption="" if not msg.caption else msg.caption.html, filename=msg.document.file_name)
-                 else:
+               else:
                     caption = "" if not msg.caption else msg.caption.html
 
                 if DISABLE_CHANNEL_BUTTON:
