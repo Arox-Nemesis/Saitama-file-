edce9e3f846ad7b5c21d6bc408c535f2e0fcfdd1
Arox-Nemesis <135242263+Arox-Nemesis@users.noreply.github.com>
2025-03-20 18:24:08 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -7,6 +7,7 @@ import re
 import string
 import time
 import asyncio
+import os 
 from pyrogram import Client, filters, __version__
 from pyrogram.enums import ParseMode
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
@@ -22,7 +23,7 @@ from config import (
     PROTECT_CONTENT,
     OWNER_ID,
 )
-from helper_func import subscribed, encode, decode, get_messages, subsch1, subsch2, subsch3 
+from helper_func import subscribed, encode, decode, get_messages, 
 from database.database import add_user, del_user, full_userbase, present_user
 from shortzy import Shortzy
 
@@ -41,66 +42,77 @@ async def start_command(client: Client, message: Message):
                 await add_user(id)
             except:
                 pass
-                text = message.text
-    if len(text)>7:
+
+    text = message.text
+    if len(text) > 7:  # Indentation fixed
         try:
-            base64_string = text.split(" ", 1)[1]
-        except:
-            return
-        _string = await decode(base64_string)
-        argument = _string.split("-")
+            base64_string = message.text.split(" ", 1)[1]
+            _string = await decode(base64_string)
+            argument = _string.split("-")
+
             if len(argument) == 3:
                 try:
                     start = int(int(argument[1]) / abs(client.db_channel.id))
                     end = int(int(argument[2]) / abs(client.db_channel.id))
                 except:
                     return
-                if start <= end:
-                    ids = range(start, end+1)
-                else:
-                    ids = []
+
+                ids = range(start, end + 1) if start <= end else []
+                if start > end:
                     i = start
-                    while True:
+                    while i >= end:
                         ids.append(i)
                         i -= 1
-                        if i < end:
-                            break
+
             elif len(argument) == 2:
                 try:
                     ids = [int(int(argument[1]) / abs(client.db_channel.id))]
                 except:
                     return
+
             temp_msg = await message.reply("Please wait...")
             try:
                 messages = await get_messages(client, ids)
             except:
                 await message.reply_text("Something went wrong..!")
                 return
-            await temp_msg.delete()
-        
-            snt_msgs = []
-        
-            for msg in messages:
-                if bool(CUSTOM_CAPTION) and bool(msg.document):
-                    CUSTOM_CAPTION.format(previouscaption="" if not msg.caption else msg.caption.html, filename=msg.document.file_name)
-               else:
-                    caption = "" if not msg.caption else msg.caption.html
 
-                if DISABLE_CHANNEL_BUTTON:
-                reply_markup = msg.reply_markup
-               else:
-                   reply_markup = None
+            await temp_msg.delete()
+            snt_msgs = []
+
+            for msg in messages:
+                caption = (
+                    CUSTOM_CAPTION.format(previouscaption="" if not msg.caption else msg.caption.html, 
+                                          filename=msg.document.file_name)
+                    if bool(CUSTOM_CAPTION) and bool(msg.document)
+                    else ("" if not msg.caption else msg.caption.html)
+                )
+
+                reply_markup = None if DISABLE_CHANNEL_BUTTON else msg.reply_markup
+
                 try:
-                    snt_msg = await msg.copy(chat_id=message.from_user.id, caption=caption, parse_mode=ParseMode.HTML, reply_markup=reply_markup, protect_content=PROTECT_CONTENT)
+                    snt_msg = await msg.copy(
+                        chat_id=message.from_user.id, 
+                        caption=caption, 
+                        parse_mode=ParseMode.HTML, 
+                        reply_markup=reply_markup, 
+                        protect_content=PROTECT_CONTENT
+                    )
                     await asyncio.sleep(0.5)
                     snt_msgs.append(snt_msg)
                 except FloodWait as e:
                     await asyncio.sleep(e.x)
-                    snt_msg = await msg.copy(chat_id=message.from_user.id, caption=caption, parse_mode=ParseMode.HTML, reply_markup=reply_markup, protect_content=PROTECT_CONTENT)
+                    snt_msg = await msg.copy(
+                        chat_id=message.from_user.id, 
+                        caption=caption, 
+                        parse_mode=ParseMode.HTML, 
+                        reply_markup=reply_markup, 
+                        protect_content=PROTECT_CONTENT
+                    )
                     snt_msgs.append(snt_msg)
                 except:
                     pass
-                    
+
             SD = await message.reply_text("Baka! Files will be deleted After 600 seconds. Save them to the Saved Message now!")
             await asyncio.sleep(600)
 
@@ -137,11 +149,11 @@ REPLY_ERROR = "<code>Use this command as a reply to any telegram message without
 async def not_joined(client: Client, message: Message):
     buttons = [
         [
-            InlineKeyboardButton(text="• ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ", url=client.invitelink1),
-            InlineKeyboardButton(text="ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ •", url=client.invitelink2),
+            InlineKeyboardButton(text="• ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ", url=client.invitelink2),
+            InlineKeyboardButton(text="ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ •", url=client.invitelink3),
         ],
         [
-            InlineKeyboardButton(text="• ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ •", url=client.invitelink3),
+            InlineKeyboardButton(text="• ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ •", url=client.invitelink),
         ]
     ]
     
@@ -158,12 +170,12 @@ async def not_joined(client: Client, message: Message):
         pass
 
     await message.reply(
-        text = FORCE_MSG.format(
-            first = message.from_user.first_name,
-            last = message.from_user.last_name,
-            username = None if not message.from_user.username else '@' + message.from_user.username,
-            mention = message.from_user.mention,
-            id = message.from_user.id
+        text=FORCE_MSG.format(
+            first=message.from_user.first_name,
+            last=message.from_user.last_name,
+            username=None if not message.from_user.username else '@' + message.from_user.username,
+            mention=message.from_user.mention,
+            id=message.from_user.id
         ),
         reply_markup=InlineKeyboardMarkup(buttons),
         quote=True,
@@ -183,11 +195,7 @@ async def send_text(client: Bot, message: Message):
     if message.reply_to_message:
         query = await full_userbase()
         broadcast_msg = message.reply_to_message
-        total = 0
-        successful = 0
-        blocked = 0
-        deleted = 0
-        unsuccessful = 0
+        total, successful, blocked, deleted, unsuccessful = 0, 0, 0, 0, 0
         
         pls_wait = await message.reply("<i>ʙʀᴏᴀᴅᴄᴀꜱᴛ ᴘʀᴏᴄᴇꜱꜱɪɴɢ ᴛɪʟʟ ᴡᴀɪᴛ ʙʀᴏᴏ... </i>")
         
@@ -214,3 +222,4 @@ async def send_text(client: Bot, message: Message):
 
     else:
         await message.reply(REPLY_ERROR)
+        
